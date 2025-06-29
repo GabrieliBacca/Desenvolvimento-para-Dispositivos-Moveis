2dcbcf55a01c44d45aad0006e26bf0aed02732ad
GabrieliBacca <baccagabrieli@gmail.com>
2025-06-29 10:56:12 -0300

    planetas

---
+++ lista-planetas/lista-planetas/app/src/main/java/MainActivity.java
@@ -0,0 +1,55 @@
+package com.ifsc.contaclick;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    DAOPlaneta daoPlaneta;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        lv = findViewById(R.id.lv);
+
+        daoPlaneta = new DAOPlaneta();
+        PlanetaAdapter planetaAdapter = new PlanetaAdapter(this,
+                R.layout.activity_planeta_adapter,
+                daoPlaneta.planetas);
+
+        lv.setAdapter(planetaAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getApplicationContext(),AtividadeB.class);
+                //Setando objeto serializado no bundle
+                intent.putExtra("planeta",daoPlaneta.planetas.get(position));
+
+                startActivity(intent);
+
+            }
+        });
+    }
+}
