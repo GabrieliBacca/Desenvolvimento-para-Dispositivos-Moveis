a98264b2eb8b313c376bce6df9a4df5da1bafe3f
GabrieliBacca <baccagabrieli@gmail.com>
2025-06-29 10:54:46 -0300

    Fragmentos

---
+++ fragmentos/app/src/main/java/MainActivity.java
@@ -0,0 +1,51 @@
+package com.ifsc.contaclick;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+    Fragment fragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button bA = findViewById(R.id.buttonFragmentA);
+        Button bB = findViewById(R.id.buttonFragmentoB);
+
+        bA.setOnClickListener(e ->{
+            fragment = new FragmentA();
+            abreFragmento();
+        });
+
+        bB.setOnClickListener(e->{
+            fragment = new FragmentB();
+            abreFragmento();
+        });
+    }
+
+    public void abreFragmento(){
+        if (this.fragment != null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame,fragment);
+            transaction.commit();
+        }
+
+    }
+}
